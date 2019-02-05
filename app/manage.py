5f244ae60ef10e19a9b1a55d5f3842146ca8c828
kelvinrandu <randukelvin@gmail.com>
2019-02-05 17:43:17 +0300

    removed sqlalchemy

--- app/manage.py
+++ app/manage.py
@@ -1,26 +1,15 @@
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from app.resources.views import telco
 from config import APP_CONFIG
 
 
-
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(APP_CONFIG[config_name])
 
     app.register_blueprint(telco)
-    
-
-    app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
-
 
 
     return app
